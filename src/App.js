ddb1c4555c5859d239077ef0517558db7194d484
ianmcpeek <iansmcpeek@gmail.com>
2025-09-20 11:40:01 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+              <img style={{
+          width: '800px'
+        }} src={process.env.PUBLIC_URL + 'jordan-birthday.png'} alt="" />
+  <a  style={{display:'block', position: 'absolute', top: '650px', left: 'calc(50% - 200px)', width: '400px', height: '200px', background: 'transparent'}}href='https://tinyurl.com/5n7j4d9p'></a>
+    </div>
+  );
+}
+
+export default App;
